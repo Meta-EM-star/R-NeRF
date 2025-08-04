bc7056678e8aec2115e035b7a83ab288b800bf95
Meta-EM <150805157+Meta-EM-star@users.noreply.github.com>
2025-08-04 10:09:37 +0800

    Update R_NeRF.py

--- Simulation data experiments/R_NeRF.py
+++ Simulation data experiments/R_NeRF.py
@@ -200,10 +200,10 @@ class RNeRF_Runner():
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='configs/mimo-csi.yml', help='config file path')
+    parser.add_argument('--config', type=str, default='configs/config.yml', help='config file path')
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--mode', type=str, default='train')
-    parser.add_argument('--dataset_type', type=str, default='config.yml')
+    parser.add_argument('--dataset_type', type=str, default='ble')
     args = parser.parse_args()
     print(args)
     torch.cuda.set_device(args.gpu)
